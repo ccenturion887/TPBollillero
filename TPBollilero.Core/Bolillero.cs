cb9e2671ca5fda2d67db692411ac41e46d1a4918
ccenturion887 <ccenturion887@gmail.com>
2022-04-13 15:36:20 -0300

    xd

---
+++ TPBollilero.Core/Bolillero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TPBollilero.Core;
+public class Bolillero
+{
+    public List<byte> Adentro {get ; set ;}
+    public List<byte> Afuera {get ; set; }
+    private IAzar azar;
+    public Bolillero()
+    {
+        Adentro = new List<byte>();
+        Afuera = new List<byte>();
+    }
+    private void crearBolilla()
+}
---
+++ TPBollilero.Core/IAzar.cs
@@ -0,0 +1,6 @@
+using System;
+namespace TPBollilero.Core;
+public class IAzar
+{
+
+}
