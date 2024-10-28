a0ae0cd294bb0b171cffa3d56e49beae40c44def
ccenturion887 <83706206+ccenturion887@users.noreply.github.com>
2024-10-28 11:11:46 +0100

    Update Bolillero.cs

--- TPBollilero.Core/Bolillero.cs
+++ TPBollilero.Core/Bolillero.cs
@@ -1,26 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-namespace TPBollilero.Core;
+using System.Linq;
+
 public class Bolillero
 {
     private List<int> bolillas;
-    public List<byte> Adentro {get ; set ;}
-    public List<byte> Afuera {get ; set; }
-    public Bolilla int {get ; set;}
     private IAzar azar;
-    public Bolillero()
+
+    public Bolillero(int cantidadBolillas, IAzar azar)
     {
-        Adentro = new ();
-        Afuera = new ();
+        bolillas = Enumerable.Range(0, cantidadBolillas).ToList();
+        this.azar = azar;
     }
-    private void crearBolilla (Adentro adentro)
+
+    public int SacarBolilla()
     {
-        if(value>=9)
-        {    
-            trhow new ArgumentException("Cantidad maxima de bolillas")
+        if (bolillas.Count == 0) throw new InvalidOperationException("No hay bolillas en el bolillero.");
+        
+        int index = azar.Next(0, bolillas.Count);
+        int bolilla = bolillas[index];
+        bolillas.RemoveAt(index);
+        return bolilla;
+    }
+
+    public void ReIngresarBolilla(int bolilla)
+    {
+        if (!bolillas.Contains(bolilla)) bolillas.Add(bolilla);
+    }
+
+    public bool Jugar(List<int> jugada)
+    {
+        List<int> bolillasSacadas = new List<int>();
+        
+        foreach (var numero in jugada)
+        {
+            bolillasSacadas.Add(SacarBolilla());
         }
-        Bolilla = value;
-        Bolilla.Add.(Bolilla);
+
+        bool acierto = bolillasSacadas.SequenceEqual(jugada);
+        foreach (var bolilla in bolillasSacadas)
+        {
+            ReIngresarBolilla(bolilla);
+        }
+
+        return acierto;
     }
-    
+
+    public int JugarNVeces(List<int> jugada, int veces)
+    {
+        int aciertos = 0;
+        for (int i = 0; i < veces; i++)
+        {
+            if (Jugar(jugada))
+            {
+                aciertos++;
+            }
+        }
+        return aciertos;
+    }
+
+    public int CantidadBolillasDentro => bolillas.Count;
+    public int CantidadBolillasFuera => 10 - bolillas.Count; // Suponiendo 10 bolillas originalmente
 }
