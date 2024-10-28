db034b555504f4de119b89355094175759661c40
ccenturion887 <83706206+ccenturion887@users.noreply.github.com>
2024-10-28 11:06:59 +0100

    Update Aleatorio.cs

--- TPBollilero.Core/Aleatorio.cs
+++ TPBollilero.Core/Aleatorio.cs
@@ -5,9 +5,8 @@ public class Aleatorio
   private List<int> bolillas;
   private Random random;
 
-  public Bolillero()
+  public int Next(int minValue, int maxValue)
     {
-        crearBolilla = Enumerable.Range(0, 10).ToList();
-        random = new Random();
+      return random.Next(minValue, maxValue);
     }
 }
